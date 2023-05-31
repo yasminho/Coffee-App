d775088a6c2ef65e9dcc5891f43f69577d7f915a
yasminho <yasminho@berkeley.edu>
2023-05-31 03:35:03 -0700

    Initial Commit

---
+++ Coffee App/Coffee_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_AppApp.swift
+//  Coffee App
+//
+//  Created by Yasmin Ho on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
